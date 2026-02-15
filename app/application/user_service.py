9f1abea7e42b35404d596320e434f5d308573459
tote10 <motialemu9@gmail.com>
2026-02-15 23:06:19 +0300

    feat: Implement user repository with functions to create a user and retrieve a user by email.

---
+++ app/application/user_service.py
@@ -0,0 +1,3 @@
+from passlib.context import CryptContext
+from app.infrastructure.user_repo import create_user
+from app.infrastructure.user_repo import get_user_by_email
--- app/infrastructure/user_repo.py
+++ app/infrastructure/user_repo.py
@@ -13,3 +13,18 @@ def create_user(email:str,password_hash:str)->User:
     cur.close()
     conn.close()
     return User(id=row[0], email=row[1], password_hash=row[2])
+def get_user_by_email(email:str):
+    conn=get_connection()
+    cur=conn.cursor()
+    cur.execute(
+        "SELECT id, email, password_hash FROM users WHERE email = %s",
+        (email,)
+    )
+    row=cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    if row :
+        return User(id=row[0], email=row[1], password_hash=row[2])
+    return None
+    
