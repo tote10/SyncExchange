3142087b239b77300973aa0018e54361794b1d5d
tote10 <motialemu9@gmail.com>
2026-02-15 23:50:40 +0300

    feat: add user registration service with password hashing

--- app/application/user_service.py
+++ app/application/user_service.py
@@ -1,3 +1,13 @@
 from passlib.context import CryptContext
 from app.infrastructure.user_repo import create_user
 from app.infrastructure.user_repo import get_user_by_email
+
+pwd_context = CryptContext(schemes = ["bcrypt"],deprecated="auto")
+
+def register_user(email:str,password:str):
+    existing_user = get_user_by_email(email)
+    if existing_user:
+        raise ValueError("User already exists")
+    hashed_password = pwd_context.hash(password)
+    user = create_user(email,hashed_password)
+    return user
