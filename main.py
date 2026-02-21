455f5be6177d3d19467ab7cb6f6c3b438ed1ce50
tote10 <motialemu9@gmail.com>
2026-02-21 03:59:19 +0300

    refactor: remove user and login router inclusions from main application setup

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ from app.presentation.user_api import router as user_router
 from app.presentation.user_api import router as login_router
 
 app = FastAPI()
-app.include_router(user_router)
-app.include_router(login_router)
