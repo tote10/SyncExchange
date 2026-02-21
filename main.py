7dedaed008106560cdf8c97e8b9deb16fcdc9a4c
tote10 <motialemu9@gmail.com>
2026-02-21 03:03:09 +0300

    refactor: separate login routes into a dedicated `login_router` and include it in the FastAPI application.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from app.presentation.user_api import router as user_router,LoginRequest
+from app.presentation.user_api import router as user_router
+
+from app.presentation.user_api import router as login_router
 
 app = FastAPI()
 app.include_router(user_router)
