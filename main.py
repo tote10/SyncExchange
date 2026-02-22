bc476531aff422b84e2e0d119df401df19dd27ee
tote10 <motialemu9@gmail.com>
2026-02-22 15:37:56 +0300

    chore: Remove redundant `login_router` import and inclusion.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from app.presentation.user_api import router as user_router
 
-from app.presentation.user_api import router as login_router
+
 
 app = FastAPI()
 app.include_router(user_router)
-app.include_router(login_router)
+
